6eb4e499c052edae9fbca1b6c50be714f5a5a19d
unknown <shahbaz342s@gmail.com>
2022-08-26 16:51:51 +0530

    fixed db url issue

--- db.js
+++ db.js
@@ -4,9 +4,7 @@ const mongoose  = require("mongoose");
 // const DB_URL = `mongodb://localhost:27017/${DBNAME}`;
 // console.log(DBNAME)
 const DB_URL = process.env.DATABASE_URL;
-// const DB_URL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.cfhzu.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
-// const DB_URL = `mongodb+srv://shahbaz3331:UvJwhGwVLuSZVkK7@cluster0.cfhzu.mongodb.net/?mernappdb=true&w=majority`;
-// const DB_URL = 'mongodb+srv://shahbaz3331:UvJwhGwVLuSZVkK7@cluster0.cfhzu.mongodb.net/?mearn_app=true&w=majority'
+
 
 const dbConnection = async () => {
    
